4753893710102bf325c0be2486daa5ade807f1db
SEELIHAT <etumanov77@gmail.com>
2024-05-04 02:43:17 -0700

    repository

---
+++ massage.js
@@ -0,0 +1,27 @@
+/*function sendMessageToWhatsApp(phoneNumber) {
+    var message = "Hello, I would like to order.";
+
+    // Replace spaces with %20 for URL
+    message = encodeURIComponent(message);
+
+    var url = "https://wa.me/" + phoneNumber + "?text=" + message;
+
+    window.open(url, '_blank');
+  }*/
+  function sendMessageToWhatsApp(phoneNumber) {
+    var message = "Hello, I would like to order.";
+  
+    // Get the text content from the <p class="order"> element
+    var additionalMessage = document.querySelector('.order').textContent;
+  
+    // Append the additional message to the main message
+    message += " " + additionalMessage;
+  
+    // Replace spaces with %20 for URL
+    message = encodeURIComponent(message);
+  
+    var url = "https://wa.me/" + phoneNumber + "?text=" + message;
+  
+    window.open(url, '_blank');
+  }
+  
