048cbafda7ad0a7e58272a8f9e0b60a5a3f702c1
SEELIHAT <etumanov77@gmail.com>
2024-05-10 00:38:48 -0700

    js change

--- massage.js
+++ massage.js
@@ -8,7 +8,7 @@
 
     window.open(url, '_blank');
   }*/
-  function sendMessageToWhatsApp(phoneNumber) {
+  /*function sendMessageToWhatsApp(phoneNumber) {
     var message = "Hello, I would like to order.";
   
     // Get the text content from the <p class="order"> element
@@ -23,5 +23,16 @@
     var url = "https://wa.me/" + phoneNumber + "?text=" + message;
   
     window.open(url, '_blank');
-  }
-  
+  }*/
+  function sendMessageToWhatsApp(phoneNumber, message) {
+    var whatsappMessage = encodeURIComponent(message);
+    window.open("https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + whatsappMessage, "_blank");
+}
+
+document.querySelectorAll('.buttt').forEach(button => {
+    button.addEventListener('click', function() {
+        let messageType = this.getAttribute('type');
+        let message = this.parentNode.querySelector('.order').textContent.replace('Order:', '');
+        sendMessageToWhatsApp('+6281338413449', message);
+    });
+});
